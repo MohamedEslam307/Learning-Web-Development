73eaed18e8ceafdcd0f163ed25eb798a6dda1535
Mohamed Eslam <mohamedeslamfekry307@example.com>
2025-02-12 19:35:42 +0200

    completed the 4th assignment

--- JavaScript/04 - Fourth Assignment/scripts.js
+++ JavaScript/04 - Fourth Assignment/scripts.js
@@ -1,152 +1,38 @@
-class Person{
-    name;
-    age;
-    gender;
-    
-    constructor(name,gender,age){
-        //make it abstract class
-        if(this.constructor === Person){
-            throw new Error("You cannot create an instance of person class");
-        }
-        this.name = name;
-        this.age = age;
-        this.gender=gender;
-    }
-    show(){
-        console.log(`Name: ${this.name}, Age: ${this.age}, Gender: ${this.gender}`);
-    }
 
-    set setName(name){
-        this.name = name;
-    }
-    get getName(){
-        return this.name;
-    }
+"use strict";
 
-    set setAge(age){
-        this.age = age;
-    }
-    get getAge(){
-        return this.age;
-    }
 
-    set setGender(gender){
-        this.gender=gender;
-    }
-    get getGender(){
-        return this.gender;
-    }
+
+function showRandomTip(){
+    let tips = [
+        "Always declare variables with var, let, or const.",
+        "Use === instead of == for comparison.",
+        "Use strict mode to catch common coding mistakes.",
+        "Keep your code DRY (Don't Repeat Yourself).",
+        "Use meaningful variable and function names.",
+        "Comment your code to explain complex logic.",
+        "Use template literals for string concatenation.",
+        "Avoid global variables to prevent conflicts.",
+        "Use arrow functions for concise syntax.",
+        "Always handle errors with try-catch blocks."
+    ];
+
+    let n=Math.floor(Math.random()*tips.length);
+    document.getElementById('tipDisplay').innerText = tips[n];
 }
 
-class Employee extends Person{
-    empId;
-    salary;
-    static employeesNumber = 0;
-    
-    constructor (name,gender,age,empId,salary){
-        super(name,gender,age);
-        this.empId = empId;
-        this.salary = salary;
-        this.employeesNumber++;
-    }
-    show(){
-        super.show();
-        console.log(`EmpId: ${this.empId}, Salary: ${this.salary}`);
-    }
-    set setEmpId(empId){
-        this.empId = empId;
-    }
-    get getEmpId(){
-        return this.empId;
-    }
-
-    set setSalary(salary){
-        this.salary = salary;
-    }
-    get getSalary(){
-        return this.salary;
-    }
-
-    static displayRules(){
-        console.log("Employee Rules");
-        
-        console.log("1. Be punctual.");
-        console.log("2. Maintain professionalism.");
-        console.log("3. Adhere to company policies.");
-        console.log("4. Respect colleagues.");
-        console.log("5. Meet deadlines.");
-
-
-    }
+function showTheDate(){
+    let date = new Date();
+    document.getElementById('dateDisplay').innerText = date.toLocaleString();
 }
 
-SalaryMixin = {
-    showSalary() {
-        console.log(`Salary: ${this.salary}`);
-    },
-    increaseSalary(amount) {
-        this.salary += amount;
-        console.log(`New Salary: ${this.salary}`);
-    }
-};
 
-Object.assign(Employee.prototype, SalaryMixin);
-
-class Course{
-    courseId;
-    courseName;
-    constructor(courseId,courseName){
-        this.courseId = courseId;
-        this.courseName = courseName;
-        this.courseDuration = courseDuration;
-    }
-    show(){
-        console.log(`CourseId: ${this.courseId}, CourseName: ${this.courseName}`);
-    }
-    set setCourseId(courseId){
-        this.courseId = courseId;
-    }
-    get getCourseId(){
-        return this.courseId;
-    }
-
-    set setCourseName(courseName){
-        this.courseName = courseName;
-    }
-    get getCourseName(){
-        return this.courseName;
-    }
-
+function isValidFullName(fullName) {
+    let fullNameRegex = /^([A-Za-z]{3,})(\s[A-Za-z]{3,})*$/;
+    return fullNameRegex.test(fullName);
 }
 
-class Student extends Person{
-    studentId;
-    marks;
-    courses = [];
-    constructor(name,gender,age,studentId,marks){
-        super(name, gender, age);
-        if(this.constructor != Student){
-            throw new Error("You cannot inherate from student class");
-        }
-        this.studentId = studentId;
-        this.marks = marks;
-    }
-    show(){
-        super.show();
-        console.log(`StudentId: ${this.studentId}, Marks: ${this.marks}`);
-    }
-    set setStudentId(studentId){
-        this.studentId = studentId;
-    }
-    get getStudentId(){
-        return this.studentId;
-    }
-
-    set setMarks(marks){
-        this.marks = marks;
-    }
-    get getMarks(){
-        return this.marks;
-    }
-
+function isValidEmail(email) {
+    let emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.(com\.eg|net\.eg|edu\.eg|org\.eg)$/;
+    return emailRegex.test(email);
 }
