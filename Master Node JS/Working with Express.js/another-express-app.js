295449c48abe56736b7bb9ad0595ec3ee44d92ca
Mohamed Eslam <mohamedeslamfekry307@example.com>
2025-02-26 16:04:37 +0200

    Enhanced the front end look

--- Master Node JS/Working with Express.js/another-express-app.js
+++ Master Node JS/Working with Express.js/another-express-app.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(adminRoutes);
 app.use(userRoutes);
+
 app.use((req,res,err)=>{
     res.status(404).sendFile( path.join(__dirname, 'views','page-not-found.html'));
 })
