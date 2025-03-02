1a5f3013ff5f93e40dba593dea9a27326f2c59f2
Mohamed Eslam <mohamedeslamfekry307@example.com>
2025-03-02 11:47:52 +0200

    removed the copied dir)

--- Master Node JS/Working with Dynamic Content (copy)/controllers/error.js
+++
@@ -1,5 +0,0 @@
-exports.get404 = (req,res,err)=>{
-    console.log('In page not found middleware');
-    res.status(404).render('page-not-found',{pageTitle: 'Page Not Found'});
-    return res.end();
-}
